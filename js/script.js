f5d62c8ec1043dce99b244ce1447ed7e3ef715cb
Lisandra Bautista <lsndra.br@gmail.com>
2023-04-04 13:58:23 -0400

    Final polishes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("form");
+  const termsCheckbox = document.getElementById("terms-checkbox");
+  const termsErrorMsg = document.getElementById("terms-error-msg");
   const dialog = document.getElementById("dialog");
   const dialogClose = document.getElementById("dialog-close");
   const dialogTitle = document.getElementById("dialog-title");
   const dialogDescription = document.getElementById("dialog-description");
 
   /**
+   * If checkbox is not checked, show error message. This is needed because
+   * the required attribute doesn't seem to work properly with form validation.
+   * @returns {boolean} True if checkbox is checked, false if not
+   */
+  const validateCheckbox = () => {
+    if (!termsCheckbox.checked) {
+      termsErrorMsg.style.display = "inline";
+    } else {
+      termsErrorMsg.style.display = "none";
+    }
+
+    return termsCheckbox.checked;
+  };
+
+  /**
    * Open dialog when submitting form, and replace placeholders with user input
    */
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    if (!validateCheckbox()) {
+      return;
+    }
+
     const name = document.getElementById("name").value;
     const firstName = name.split(" ")[0];
     const email = document.getElementById("email").value;
@@ -40,6 +62,15 @@ document.addEventListener("DOMContentLoaded", function () {
   dialog.addEventListener("click", function (event) {
     if (event.target === dialog) {
       dialog.classList.add("hidden");
+    }
+  });
+
+  /**
+   * Remove potential error message when checking the checkbox
+   */
+  termsCheckbox.addEventListener("change", () => {
+    if (termsCheckbox.checked) {
+      termsErrorMsg.style.display = "none"; // Hide the error message
     }
   });
 });
